ba23b96d5a97a45c097235ca20465ae50a98d1d5
Jan Kotanski <jankotan@gmail.com>
2019-06-20 11:34:41 +0200

    add H5PYdumpdiff filter

--- lavuefilters/h5pyplugins.py
+++ lavuefilters/h5pyplugins.py
@@ -49,7 +49,7 @@ class H5PYdump(object):
             maxindex = 1000
 
         #: (:obj: `int`) maximal image number
-        self.__maxindex = maxindex
+        self._maxindex = maxindex
 
         #: (:obj: `str` >) list of indexes for gap
         self.__filename = configuration \
@@ -57,43 +57,87 @@ class H5PYdump(object):
             else "/tmp/lavueh5pydump.h5"
 
         self.__grpindex = 0
-        self.__imgindex = 0
-        self.__h5entry = None
-        self.__h5data = None
-        self.__h5field = None
+        self._imgindex = 0
+        self._h5entry = None
+        self._h5data = None
+        self._h5field = None
 
-        self.__h5file = h5py.File(self.__filename, 'w', libver='latest')
+        self._h5file = h5py.File(self.__filename, 'w', libver='latest')
 
     def _add_new_entry(self):
         """ add a new scan entry
         """
         self.__grpindex += 1
-        self.__h5entry = self.__h5file.create_group(
+        self._h5entry = self._h5file.create_group(
             "scan_%s" % self.__grpindex)
-        self.__h5entry.attrs["NX_class"] = "NXentry"
-        self.__h5data = self.__h5entry.create_group("data")
-        self.__h5data.attrs["NX_class"] = "NXdata"
-        self.__h5field = None
+        self._h5entry.attrs["NX_class"] = "NXentry"
+        self._h5data = self._h5entry.create_group("data")
+        self._h5data.attrs["NX_class"] = "NXdata"
+        self._h5field = None
 
     def _reopen(self):
         """  reopen the file
         """
-        self.__h5file.close()
-        self.__h5file = h5py.File(self.__filename, 'r+', libver='latest')
-        self.__h5file.swmr_mode = True
-        self.__h5entry = self.__h5file.get("scan_%s" % self.__grpindex)
-        self.__h5data = self.__h5entry.get("data")
-        self.__h5field = self.__h5data.get("data")
+        self._h5file.close()
+        self._h5file = h5py.File(self.__filename, 'r+', libver='latest')
+        self._h5file.swmr_mode = True
+        self._h5entry = self._h5file.get("scan_%s" % self.__grpindex)
+        self._h5data = self._h5entry.get("data")
+        self._h5field = self._h5data.get("data")
 
     def _reset(self):
         """  remove the file and create a new one
         """
-        self.__h5file.close()
-        self.__h5file = h5py.File(self.__filename, 'w', libver='latest')
-        self.__h5entry = None
-        self.__h5data = None
-        self.__h5field = None
-        self.__imgindex = 0
+        self._h5file.close()
+        self._h5file = h5py.File(self.__filename, 'w', libver='latest')
+        self._h5entry = None
+        self._h5data = None
+        self._h5field = None
+        self._imgindex = 0
+        self.__grpindex -= 1
+
+    def _check_shape_and_dtype(self, image):
+        """ reset the field if its shape or type has changed
+
+        :param image: numpy array with an image
+        :type image: :class:`numpy.ndarray`
+        """
+        if self._h5field is not None:
+            if self._h5field.shape[1:] != image.shape or \
+               self._h5field.dtype != image.dtype:
+                self._h5field = None
+
+    def _create_entry_and_field(self, image):
+        """
+        """
+        self._add_new_entry()
+        shape = [0]
+        shape.extend(image.shape)
+        chunk = [1]
+        chunk.extend(image.shape)
+        dtype = image.dtype
+        maxshape = [None] * len(shape)
+        self._h5field = self._h5data.create_dataset(
+            "data",
+            shape=tuple(shape),
+            chunks=tuple(chunk),
+            dtype=dtype,
+            maxshape=tuple(maxshape))
+        self._reopen()
+
+    def _append_image(self, image):
+        """ appends the image
+
+        :param image: numpy array with an image
+        :type image: :class:`numpy.ndarray`
+        """
+
+        new_shape = list(self._h5field.shape)
+        new_shape[0] += 1
+        self._h5field.resize(tuple(new_shape))
+        self._h5field[-1, :, :] = np.transpose(image)
+        if hasattr(self._h5field, "flush"):
+            self._h5field.flush()
 
     def __call__(self, image, imagename, metadata, imagewg):
         """ call method
@@ -109,32 +153,49 @@ class H5PYdump(object):
         :returns: numpy array with an image
         :rtype: :class:`numpy.ndarray` or `None`
         """
-        self.__imgindex += 1
-        if self.__maxindex > 0 and self.__imgindex >= self.__maxindex:
+        self._imgindex += 1
+        if self._maxindex > 0 and self._imgindex >= self._maxindex:
             self._reset()
-        if self.__h5field is not None:
-            if self.__h5field.shape[1:] != image.shape or \
-               self.__h5field.dtype != image.dtype:
-                self.__h5field = None
-        if self.__h5field is None:
-            self._add_new_entry()
-            shape = [0]
-            shape.extend(image.shape)
-            chunk = [1]
-            chunk.extend(image.shape)
-            dtype = image.dtype
-            maxshape = [None] * len(shape)
-            self.__h5field = self.__h5data.create_dataset(
-                "data",
-                shape=tuple(shape),
-                chunks=tuple(chunk),
-                dtype=dtype,
-                maxshape=tuple(maxshape))
-            self._reopen()
+        self._check_shape_and_dtype(image)
+        if self._h5field is None:
+            self._create_entry_and_field(image)
+        self._append_image(image)
 
-        new_shape = list(self.__h5field.shape)
-        new_shape[0] += 1
-        self.__h5field.resize(tuple(new_shape))
-        self.__h5field[-1, :, :] = np.transpose(image)
-        if hasattr(self.__h5field, "flush"):
-            self.__h5field.flush()
+
+class H5PYdumpdiff(H5PYdump):
+
+    """ Horizontal gap filter"""
+
+    def __init__(self, configuration=None):
+        """ constructor
+
+        :param configuration: file name to dump images and,  max image numbers
+        :type configuration: :obj:`str`
+        """
+        H5PYdump.__init__(self, configuration)
+        self.__lastimage = None
+
+    def __call__(self, image, imagename, metadata, imagewg):
+        """ call method
+
+        :param image: numpy array with an image
+        :type image: :class:`numpy.ndarray`
+        :param imagename: image name
+        :type imagename: :obj:`str`
+        :param metadata: JSON dictionary with metadata
+        :type metadata: :obj:`str`
+        :param imagewg: image wigdet
+        :type imagewg: :class:`lavuelib.imageWidget.ImageWidget`
+        :returns: numpy array with an image
+        :rtype: :class:`numpy.ndarray` or `None`
+        """
+        if self.__lastimage is None or \
+           not np.array_equal(self.__lastimage, image):
+            self._imgindex += 1
+            if self._maxindex > 0 and self._imgindex >= self._maxindex:
+                self._reset()
+            self._check_shape_and_dtype(image)
+            if self._h5field is None:
+                self._create_entry_and_field(image)
+            self._append_image(image)
+        self.__lastimage = np.array(image)
